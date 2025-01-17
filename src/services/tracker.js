1bebf20c9a0e2aef55e9b7d2159bc8b7b75d4804
porcelaincode <allstarworkburner@gmail.com>
2025-01-17 09:19:49 +0530

    updated open replay tracker for better tracking ability

--- src/services/tracker.js
+++ src/services/tracker.js
@@ -3,7 +3,14 @@ import Tracker from '@openreplay/tracker';
 // Create and configure the tracker
 const tracker = new Tracker({
   projectKey: import.meta.env.VITE_OPENREPLAY_PROJECT_KEY,
-  __DISABLE_SECURE_MODE: import.meta.env.DEV,
+  __DISABLE_SECURE_MODE: !import.meta.env.DEV,
+  obscureInputDates: false,
+  defaultInputMode: 0,
+  obscureInputEmails: false,
+  obscureInputNumbers: false,
+  obscureTextNumbers: false,
+  obscureTextEmails: false,
+  capturePerformance: true
 });
 
 export default tracker;
